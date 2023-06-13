2243cce3b44b44d440ef6126b121ab3709be9023
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-13 12:21:05 -0600

    Declaracion y uso inicial de un arreglo unidimensional float

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 int main(void){
-    int V[10];//Arreglo para almacenar hasta 10 datos int
+    float V[10];//Arreglo para almacenar hasta 10 datos float
 
-    V[0] = 1;//Se almacena, en la posicion 0, directamente un dato int
+    V[0] = 1.2;//Se almacena, en la posicion 0, directamente un dato float
 
 
-    printf("Ingresa un valor int negativo ");
-    scanf("%d",&V[3]);//Se almacena, en la posicion 3, un dato int ingresado por teclado
+    printf("Ingresa un valor float negativo ");
+    scanf("%f",&V[3]);//Se almacena, en la posicion 3, un dato float ingresado por teclado
 
-    V[6] = V[0] + V[3];//Se almacena, en la posicion 6, el resultado int
-                        //de sumar el dato int en la posicion 0
-                        //con el dato int en la posicion 3
+    V[6] = V[0] + V[3];//Se almacena, en la posicion 6, el resultado float
+                        //de sumar el dato float en la posicion 0
+                        //con el dato float en la posicion 3
 
-    V[9] = abs(V[6]);//Se almacena, en la posicion 9, el valor absoluto int
+    V[9] = fabs(V[6]);//Se almacena, en la posicion 9, el valor absoluto float
                         //que retorna la funcion abs,
-                        //tras enviarle el valor int en la posicion 6
+                        //tras enviarle el valor float en la posicion 6
 
-    printf("%d + %d = %d\n", V[0], V[3], V[6]);
-    printf("|%d| = %d\n", V[6], V[9]);
+    printf("%f + %f = %f\n", V[0], V[3], V[6]);
+    printf("|%f| = %f\n", V[6], V[9]);
 
     return 0;
 }
