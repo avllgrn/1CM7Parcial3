c7acbf64b69ff32036623bad4713f36f9054cd6a
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-13 12:23:10 -0600

    Se pide y muestra un vector de 10 eementos float

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main(void){
     float V[10];//Arreglo para almacenar hasta 10 datos float
+    int i;
 
-    V[0] = 1.2;//Se almacena, en la posicion 0, directamente un dato float
+    //Se muestra el contenido inicial (basura) de los diez elementos del arreglo,
+    //posicion por posicion, de la 0 a la 10 (sin llegar a 10)
+    for(i=0; i<10; i++)
+        printf("V[%d] = %f\n",i,V[i]);
 
+    system("pause");
+    system("cls");
 
-    printf("Ingresa un valor float negativo ");
-    scanf("%f",&V[3]);//Se almacena, en la posicion 3, un dato float ingresado por teclado
+    //Se pide al usuario cada uno de los diez elementos del arreglo,
+    //posicion por posicion, de la 0 a la 10 (sin llegar a 10)
+    for(i=0; i<10; i++){
+        printf("Ingresa V[%d] ",i);
+        scanf("%f",&V[i]);
+    }
 
-    V[6] = V[0] + V[3];//Se almacena, en la posicion 6, el resultado float
-                        //de sumar el dato float en la posicion 0
-                        //con el dato float en la posicion 3
+    system("pause");
+    system("cls");
 
-    V[9] = fabs(V[6]);//Se almacena, en la posicion 9, el valor absoluto float
-                        //que retorna la funcion abs,
-                        //tras enviarle el valor float en la posicion 6
-
-    printf("%f + %f = %f\n", V[0], V[3], V[6]);
-    printf("|%f| = %f\n", V[6], V[9]);
+    //Se muestra el contenido final de los diez elementos del arreglo,
+    //posicion por posicion, de la 0 a la 10 (sin llegar a 10)
+    for(i=0; i<10; i++)
+        printf("V[%d] = %f\n",i,V[i]);
 
     return 0;
 }
