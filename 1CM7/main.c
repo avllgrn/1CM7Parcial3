532ad04942968e0395b480d8f6c971892d96d98c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-18 15:55:35 -0600

    Suma y resta de vectores con funciones

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,29 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void leeVector(float X[50], int n);
+void rellenaVector(float X[50], int n);
 void muestraVector(float X[50], int n);
+void sumaVectores(float X[50], float Y[50], float Z[50], int n);
+void restaVectores(float X[50], float Y[50], float Z[50], int n);
 
 int main(void){
-    float V[50];
-    int n;
+    float V1[50], V2[50], V3[50], V4[50];
+    int n1, n2, n3, n4;
+    srand(time(NULL));//Se inicializa la funcion para numeros aleatorios.
 
-    printf("Cuantos elementos tendra V? ");
-    scanf("%d",&n);
-    if(n<=0 || n>50)
-        printf("Dimension invalida...\n\n");
+    printf("Cuantos elementos tendra V1? ");
+    scanf("%d",&n1);
+    printf("Cuantos elementos tendra V2? ");
+    scanf("%d",&n2);
+
+    if(n1<=0 || n1>50 ||
+       n2<=0 || n2>50 )
+        printf("Dimensiones invalidas...\n\n");
+    else if(n1 != n2)
+        printf("Error! No pueden sumarse ni restarse...\n\n");
     else{
-        printf("Ingresa V\n");
-        leeVector(V,n);
-        system("pause");
-        system("cls");
+        n3 = n1;
+        n4 = n2;
 
-        printf("V\n");
-        muestraVector(V,n);
+        rellenaVector(V1,n1);
+        rellenaVector(V2,n2);
+
+        sumaVectores(V1,V2,V3,n3);
+        restaVectores(V1,V2,V4,n4);
+
+        printf("\nV1\n");
+        muestraVector(V1,n1);
+        printf("\nV2\n");
+        muestraVector(V2,n2);
+        printf("\nV1+V2\n");
+        muestraVector(V3,n3);
+        printf("\nV1-V2\n");
+        muestraVector(V3,n3);
     }
 
     return 0;
 }
+
 void leeVector(float X[50], int n){
     int i;
     for(i=0; i<n; i++){
@@ -31,8 +53,23 @@ void leeVector(float X[50], int n){
         scanf("%f",&X[i]);
     }
 }
+void rellenaVector(float X[50], int n){
+    int i;
+    for(i=0; i<n; i++)
+        X[i] = rand() % 100;
+}
 void muestraVector(float X[50], int n){
     int i;
     for(i=0; i<n; i++)
         printf("[%d] = %f\n",i,X[i]);
+}
+void sumaVectores(float X[50], float Y[50], float Z[50], int n){
+    int i;
+    for(i=0; i<n; i++)
+        Z[i] = X[i] + Y[i];
+}
+void restaVectores(float X[50], float Y[50], float Z[50], int n){
+    int i;
+    for(i=0; i<n; i++)
+        Z[i] = X[i] - Y[i];
 }
