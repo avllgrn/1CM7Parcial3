819d61109e6673370bc83ec182e000ea8ae515ca
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-18 15:38:17 -0600

    Suma de dos vectores

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -3,25 +3,35 @@
 #include <time.h>
 
 int main(void){
-    float V[50];//Se reserva un vector de haste 50 elementos
-    int i, n;
+    float V1[50], V2[50], V3[50];
+    int i, n1, n2, n3;
     srand(time(NULL));//Se inicializa la funcion para numeros aleatorios.
 
-    printf("Cuantos elementos tendra V? ");
-    scanf("%d",&n);
+    printf("Cuantos elementos tendra V1? ");
+    scanf("%d",&n1);
+    printf("Cuantos elementos tendra V2? ");
+    scanf("%d",&n2);
 
-    if(n>0 && n<=50){//Se utilizan solo los que el usuario desee,
-                     //mientras no revase el tamano del vector.
-                     //Los espacios no utilizados, se desperdician
-        for(i=0; i<n; i++)
-            V[i] = rand() % 10; //Se rellena el vector con valores aleatorios
-                                //entre [0, 10)
+    if(n1<=0 || n1>50 ||
+       n2<=0 || n2>50 )
+        printf("Dimensiones invalidas...\n\n");
+    else if(n1 != n2)
+        printf("Error! No pueden sumarse...\n\n");
+    else{
+        n3 = n1;
+        printf("%d %d %d\n\n",n1,n2,n3);
+        for(i=0; i<n1; i++)
+            V1[i] = rand() % 100; //Se rellena el vector con valores aleatorios entre [0, 100)
 
-        for(i=0; i<n; i++)
-            printf("V[%d] = %f\n",i, V[i]);
+        for(i=0; i<n2; i++)
+            V2[i] = rand() % 100; //Se rellena el vector con valores aleatorios entre [0, 100)
+
+        for(i=0; i<n3; i++)
+            V3[i] = V1[i] + V2[i]; //Se suman los arreglos, elemento a elemento
+
+        for(i=0; i<n3; i++)//Se muestra simultaneamente los tres arreglos, elemento pot elemento
+            printf("[%d]\t%f\t+\t%f\t= %f\n",i, V1[i], V2[i], V3[i]);
     }
-    else
-        printf("Dimension del arreglo invalida...\n\n");
 
     return 0;
 }
