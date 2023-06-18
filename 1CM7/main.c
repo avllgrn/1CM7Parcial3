906ccc54cd7f298e1fe9f39439bebbbd8a819049
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-18 15:46:20 -0600

    Leer y mostrar un arreglo con funciones

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
+void leeVector(float X[50], int n);
+void muestraVector(float X[50], int n);
 
 int main(void){
-    float V1[50], V2[50], V3[50];
-    int i, n1, n2, n3;
-    srand(time(NULL));//Se inicializa la funcion para numeros aleatorios.
+    float V[50];
+    int n;
 
-    printf("Cuantos elementos tendra V1? ");
-    scanf("%d",&n1);
-    printf("Cuantos elementos tendra V2? ");
-    scanf("%d",&n2);
-
-    if(n1<=0 || n1>50 ||
-       n2<=0 || n2>50 )
-        printf("Dimensiones invalidas...\n\n");
-    else if(n1 != n2)
-        printf("Error! No pueden sumarse...\n\n");
+    printf("Cuantos elementos tendra V? ");
+    scanf("%d",&n);
+    if(n<=0 || n>50)
+        printf("Dimension invalida...\n\n");
     else{
-        n3 = n1;
-        printf("%d %d %d\n\n",n1,n2,n3);
-        for(i=0; i<n1; i++)
-            V1[i] = rand() % 100; //Se rellena el vector con valores aleatorios entre [0, 100)
+        printf("Ingresa V\n");
+        leeVector(V,n);
+        system("pause");
+        system("cls");
 
-        for(i=0; i<n2; i++)
-            V2[i] = rand() % 100; //Se rellena el vector con valores aleatorios entre [0, 100)
-
-        for(i=0; i<n3; i++)
-            V3[i] = V1[i] - V2[i]; //Se restan los arreglos, elemento a elemento
-
-        for(i=0; i<n3; i++)//Se muestra simultaneamente los tres arreglos, elemento pot elemento
-            printf("[%d]\t%f\t-\t%f\t= %f\n",i, V1[i], V2[i], V3[i]);
+        printf("V\n");
+        muestraVector(V,n);
     }
 
     return 0;
+}
+void leeVector(float X[50], int n){
+    int i;
+    for(i=0; i<n; i++){
+        printf("Ingresa [%d] ",i);
+        scanf("%f",&X[i]);
+    }
+}
+void muestraVector(float X[50], int n){
+    int i;
+    for(i=0; i<n; i++)
+        printf("[%d] = %f\n",i,X[i]);
 }
