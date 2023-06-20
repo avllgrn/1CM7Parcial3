d9f8f63aade90b6c29861d07993a2a83e3261fb0
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-20 17:08:49 -0600

    Programa de calificaciones en Vectores

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -2,74 +2,63 @@
 #include <stdlib.h>
 #include <time.h>
 
-void leeVector(float X[50], int n);
-void rellenaVector(float X[50], int n);
-void muestraVector(float X[50], int n);
-void sumaVectores(float X[50], float Y[50], float Z[50], int n);
-void restaVectores(float X[50], float Y[50], float Z[50], int n);
-
 int main(void){
-    float V1[50], V2[50], V3[50], V4[50];
-    int n1, n2, n3, n4;
+    float P1[50], P2[50], P3[50], Promedio[50],mayor,menor,s,promGral;
+    int i,n,aprobados,reprobados;
     srand(time(NULL));//Se inicializa la funcion para numeros aleatorios.
 
-    printf("Cuantos elementos tendra V1? ");
-    scanf("%d",&n1);
-    printf("Cuantos elementos tendra V2? ");
-    scanf("%d",&n2);
+    printf("Cuantos alumnos tienes? ");
+    scanf("%d",&n);
 
-    if(n1<=0 || n1>50 ||
-       n2<=0 || n2>50 )
-        printf("Dimensiones invalidas...\n\n");
-    else if(n1 != n2)
-        printf("Error! No pueden sumarse ni restarse...\n\n");
-    else{
-        n3 = n1;
-        n4 = n2;
-
-        rellenaVector(V1,n1);
-        rellenaVector(V2,n2);
-
-        sumaVectores(V1,V2,V3,n3);
-        restaVectores(V1,V2,V4,n4);
-
-        printf("\nV1\n");
-        muestraVector(V1,n1);
-        printf("\nV2\n");
-        muestraVector(V2,n2);
-        printf("\nV1+V2\n");
-        muestraVector(V3,n3);
-        printf("\nV1-V2\n");
-        muestraVector(V3,n3);
+    for(i=0; i<n; i++){//Se rellenan los parciales, alumno por alumno
+        P1[i] = (rand() % 101)/10.0;
+        P2[i] = (rand() % 101)/10.0;
+        P3[i] = (rand() % 101)/10.0;
     }
+
+    for(i=0; i<n; i++)//Se calculan los promedios, alumno por alumno
+        Promedio[i] = (P1[i]+P2[i]+P3[i])/3;
+
+    //Busca la mayor calificacion de P1
+    mayor = P1[0];//Se supone que la primera calificacion es la mayor
+    for(i=0; i<n; i++)
+        if(P1[i] > mayor)//Si se encuentra una mayor,
+            mayor = P1[i];//se almacena la nueva
+
+    //Busca la menor calificacion de P2
+    menor = P2[0];//Se supone que la primera calificacion es la mayor
+    for(i=0; i<n; i++)
+        if(P2[i] < menor)//Si se encuentra una menor,
+            menor = P2[i];//se almacena la nueva
+
+    //Busca cuantos reprobaron P3
+    reprobados = 0;//Inicialmente, no se sabe cuantos reprobados hay
+    for(i=0; i<n; i++)
+        if(P3[i] < 6)//Si se encuentra una calificacion reprobatoria,
+            reprobados++;//se incrementa el conteo de reprobados
+
+    //Busca cuantos aprobaron la materia
+    aprobados = 0;//Inicialmente, no se sabe cuantos aprobados hay
+    for(i=0; i<n; i++)
+        if(Promedio[i] >= 6)//Si se encuentra una calificacion aprobatoria,
+            aprobados++;//se incrementa el conteo de aprobados
+
+    //Acumula las calificaciones finales
+    s = 0;//Suma acumulada comienza en cero
+    for(i=0; i<n; i++)
+        s = s + Promedio[i];//Acumula cada calificacion final
+    promGral = s/n;//El promedio es la suma de todos, dividido entre n alumnos
+
+
+    printf("Alumno\tP1\t\tP2\t\tP3\t\tPromedio\n\n");
+    for(i=0; i<n; i++)//Se muestran las calificaciones
+        printf("%d\t%f\t%f\t%f\t%f\n",i,P1[i],P2[i],P3[i],Promedio[i]);
+
+    printf("\nMayor calificacion en P1\t= %f\n",mayor);
+    printf("Menor calificacion en P2\t= %f\n",menor);
+    printf("Alumnos reprobados en P3\t= %d\n",reprobados);
+    printf("Alumnos aprobaron la materia\t= %d\n",aprobados);
+    printf("Promedio final del grupo\t= %f\n",promGral);
 
     return 0;
-}
-
-void leeVector(float X[50], int n){
-    int i;
-    for(i=0; i<n; i++){
-        printf("Ingresa [%d] ",i);
-        scanf("%f",&X[i]);
-    }
-}
-void rellenaVector(float X[50], int n){
-    int i;
-    for(i=0; i<n; i++)
-        X[i] = rand() % 100;
-}
-void muestraVector(float X[50], int n){
-    int i;
-    for(i=0; i<n; i++)
-        printf("[%d] = %f\n",i,X[i]);
-}
-void sumaVectores(float X[50], float Y[50], float Z[50], int n){
-    int i;
-    for(i=0; i<n; i++)
-        Z[i] = X[i] + Y[i];
-}
-void restaVectores(float X[50], float Y[50], float Z[50], int n){
-    int i;
-    for(i=0; i<n; i++)
-        Z[i] = X[i] - Y[i];
 }
