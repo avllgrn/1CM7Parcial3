386b0bc09668fd28e250781cf65dca0d8d697d35
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-22 13:10:10 -0600

    Contador dentro de contador (visto verticalmente)

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -2,50 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
-void rellenaVector(float X[50], int n);
-void muestraVector(float X[50], int n);
-void aplicaBurbuja(float X[50], int n);
-
 int main(void){
-    srand(time(NULL));
-    float V[50];
-    int n;
+    int i, j, m, n;
 
-    printf("Cuantos valores? ");scanf("%d",&n);
+    printf("Ingresa m ");scanf("%d",&m);
+    printf("Ingresa n ");scanf("%d",&n);
 
-    rellenaVector(V,n);
-    printf("V inicial\n");
-    muestraVector(V,n);
-
-    aplicaBurbuja(V,n);
-
-    printf("\nV final\n");
-    muestraVector(V,n);
+    for(i=0; i<m; i++){//Contador externo es mas lento que el interno, da m vueltas
+        for(j=0; j<n; j++){ //Contador externo es mas rapido que el externo,
+                            //da n vueltas por cada vuelta del externo (total=n x m)
+            printf("i=%d j=%d\n",i,j);
+        }
+    }
 
     return 0;
-}
-
-void rellenaVector(float X[50], int n){
-    int i;
-    for(i=0; i<n; i++)
-        X[i] = rand() % 1000;
-}
-void muestraVector(float X[50], int n){
-    int i;
-    for(i=0; i<n; i++)
-        printf("[%d] = %f\n",i,X[i]);
-}
-void aplicaBurbuja(float X[50], int n){
-    float aux;
-    int veces, i;
-    for(veces=1; veces<n; veces++){
-        for(i=0; i<n-1; i++)//Compara las posiciones contiguas
-            if(X[i] < X[i+1]){//Intercambialas, si es necesario
-                aux = X[i];
-                X[i] = X[i+1];
-                X[i+1] = aux;
-            }
-            //printf("\n\n%da vez que se aplica burbuja\n",veces);
-            //muestraVector(X,n);
-    }
 }
