1ce10fe360e2369231f68adeaafc89fe4d32018e
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-22 12:38:47 -0600

    Invertir dos valores en variables

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -2,23 +2,18 @@
 #include <stdlib.h>
 
 int main(void){
-    int x, xCociente, i, n;
-    int binario[50];
 
-    printf("Ingresa entero positivo ");
-    scanf("%d",&x);
-    printf("\n\n(%d)10 = (",x);
+    int a = 5;
+    int b = 10;
+    int aux;
 
-    for(i=0, xCociente=x; xCociente!=0; i++){
-        binario[i] = xCociente%2;
-        xCociente = xCociente/2;
-    }
-    n = i;//Numero de bits (posiciones del vector) utilizados
+    printf("a = %d\tb=%d\n\n",a,b);
 
-    for(i=n-1; i>=0; i--)
-        printf("%d",binario[i]);
+    aux=a;
+    a=b;
+    b=aux;
 
-    printf(")2\n\n");
+    printf("a = %d\tb=%d\n\n",a,b);
 
     return 0;
 }
