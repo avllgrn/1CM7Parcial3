c936b7b8d39f424d7b9fa4ddd2402b7162ecc940
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-22 13:03:34 -0600

    Ordenamiento ascendente con burbuja

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -1,19 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+void rellenaVector(float X[50], int n);
+void muestraVector(float X[50], int n);
+void aplicaBurbuja(float X[50], int n);
 
 int main(void){
+    srand(time(NULL));
+    float V[50];
+    int n;
 
-    int a = 5;
-    int b = 10;
-    int aux;
+    printf("Cuantos valores? ");scanf("%d",&n);
 
-    printf("a = %d\tb=%d\n\n",a,b);
+    rellenaVector(V,n);
+    printf("V inicial\n");
+    muestraVector(V,n);
 
-    aux=a;
-    a=b;
-    b=aux;
+    aplicaBurbuja(V,n);
 
-    printf("a = %d\tb=%d\n\n",a,b);
+    printf("\nV final\n");
+    muestraVector(V,n);
 
     return 0;
+}
+
+void rellenaVector(float X[50], int n){
+    int i;
+    for(i=0; i<n; i++)
+        X[i] = rand() % 1000;
+}
+void muestraVector(float X[50], int n){
+    int i;
+    for(i=0; i<n; i++)
+        printf("[%d] = %f\n",i,X[i]);
+}
+void aplicaBurbuja(float X[50], int n){
+    float aux;
+    int veces, i;
+    for(veces=1; veces<n; veces++){
+        for(i=0; i<n-1; i++)//Compara las posiciones contiguas
+            if(X[i] > X[i+1]){//Intercambialas, si es necesario
+                aux = X[i];
+                X[i] = X[i+1];
+                X[i+1] = aux;
+            }
+            //printf("\n\n%da vez que se aplica burbuja\n",veces);
+            //muestraVector(X,n);
+    }
 }
