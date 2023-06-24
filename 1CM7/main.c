4c65e89141bc8d1db21d82c412c1f25ecf997526
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-23 20:01:21 -0600

    Suma de dos matrices

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -3,45 +3,70 @@
 #include <time.h>
 
 int main(void){
-    int M[50][50];//Se declara matriz de ints, para 50 filas y 50 columnas
-    int MT[50][50];//Su transpuesta tambien seria, para 50 filas y 50 columnas
-    int i, j, m, n, mT, nT;
+    int M1[50][50];
+    int M2[50][50];
+    int M3[50][50];
+    int i, j, m1, n1, m2, n2, m3, n3;
     srand(time(NULL));
 
-    printf("Cuantas filas? ");
-    scanf("%d",&m);
-    printf("Cuantas columnas? ");
-    scanf("%d",&n);
-    mT = n;//Las columnas de la original seran las fila de la transpuesta
-    nT = m;//Las filas de la original seran las columnas de la transpuesta
+    printf("Cuantas filas para M1? ");
+    scanf("%d",&m1);
+    printf("Cuantas columnas para M1? ");
+    scanf("%d",&n1);
+    printf("Cuantas filas para M2? ");
+    scanf("%d",&m2);
+    printf("Cuantas columnas para M2? ");
+    scanf("%d",&n2);
 
-    for(i=0; i<m; i++){
-        for(j=0; j<n; j++){
-            M[i][j] = rand() % 100;
-        }
-    }
+    if(m1!=m2 || n1!=n2)
+        printf("\n\nError! No puede sumarse...\n\n");
+    else{
+        m3=m1;
+        n3=n2;
 
-    for(i=0; i<m; i++){//Se genera la transpuesta de M
-        for(j=0; j<n; j++){
-            MT[j][i] = M[i][j]; //Con las filas de la original, como columnas de la transpuesta
-                                //Con las columnas de la original, como filas de la transpuesta
+        for(i=0; i<m1; i++){
+            for(j=0; j<n1; j++){
+                M1[i][j] = rand() % 100;
+            }
         }
-    }
 
-    printf("\nM\n");
-    for(i=0; i<m; i++){//Se muestra de izquiera a derecha, hacia abajo
-        for(j=0; j<n; j++){
-            printf("%d\t",M[i][j]);
-        }
-        printf("\n");
-    }
+        for(i=0; i<m2; i++){
+            for(j=0; j<n2; j++){
+                M2[i][j] = rand() % 100;
 
-    printf("\nMT\n");
-    for(i=0; i<mT; i++){//Se muestra de izquiera a derecha, hacia abajo
-        for(j=0; j<nT; j++){
-            printf("%d\t",MT[i][j]);
+            }
         }
-        printf("\n");
+
+        for(i=0; i<m3; i++){
+            for(j=0; j<n3; j++){//Se suman elemento a elemento
+                M3[i][j] = M1[i][j] + M2[i][j];
+
+            }
+        }
+
+        printf("\nM1\n");
+        for(i=0; i<m1; i++){//Se muestra de izquiera a derecha, hacia abajo
+            for(j=0; j<n1; j++){
+                printf("%d\t",M1[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nM2\n");
+        for(i=0; i<m2; i++){//Se muestra de izquiera a derecha, hacia abajo
+            for(j=0; j<n2; j++){
+                printf("%d\t",M2[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nM1+M2\n");
+        for(i=0; i<m3; i++){//Se muestra de izquiera a derecha, hacia abajo
+            for(j=0; j<n3; j++){
+                printf("%d\t",M3[i][j]);
+            }
+            printf("\n");
+        }
     }
 
     return 0;
