c4f220f9de02e79aeecd093c82c00bc18370b104
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-06-27 11:49:54 -0600

    Mas ejercicios de matriz de calificaciones

--- 1CM7/main.c
+++ 1CM7/main.c
@@ -4,9 +4,11 @@
 
 int main(void){
     float Calificaciones[50][11];
-    float menor,mayor;
+    float menor,mayor,x;
     int i,j,m,n,alumnoMenor,parcialMenor,alumnoMayor,parcialMayor,
-        aprobadosParcial,reprobadosParcial;
+        aprobadosParcial,reprobadosParcial,cuantasX,parcialesAprobadosAlumno1,
+        parcialesReprobadosAlumno3,alumnosAprobadosParcial0,
+        alumnosReprobadosParcial2, reprobadosFinal;
     srand(time(NULL));
 
     printf("Cuantos alumnos? ");scanf("%d",&m);
@@ -25,8 +27,7 @@ int main(void){
         for(j=0; j<n; j++){
             Calificaciones[i][n] = Calificaciones[i][n] + Calificaciones[i][j];
         }
-        Calificaciones[i][n] =
-        Calificaciones[i][n] / n;
+        Calificaciones[i][n] = Calificaciones[i][n] / n;
     }
 
 
@@ -80,30 +81,50 @@ int main(void){
             if(Calificaciones[i][j] < 6)
                 reprobadosParcial++;
 
+    x = 10;//Esto puede preguntarsele al usuario
+    cuantasX = 0;
+    for(i=0; i<m; i++)
+        for(j=0; j<n; j++)
+            if(Calificaciones[i][j] == x)
+                cuantasX++;
 
-    printf("Calif menor del semestre: %f, alumno %d, parcial %d\n",
-           menor,alumnoMenor+1,parcialMenor+1);
-    printf("Calif mayor del semestre: %f, alumno %d, parcial %d\n",
-           mayor,alumnoMayor+1,parcialMayor+1);
+
+    parcialesAprobadosAlumno1 = 0;
+    for(j=0; j<n; j++)
+        if(Calificaciones[1][j] >= 6)
+            parcialesAprobadosAlumno1++;
+
+    parcialesReprobadosAlumno3 = 0;
+    for(j=0; j<n; j++)
+        if(Calificaciones[3][j] < 6)
+            parcialesReprobadosAlumno3++;
+
+    alumnosAprobadosParcial0 = 0;
+    for(i=0; i<m; i++)
+        if(Calificaciones[i][0] >= 6)
+            alumnosAprobadosParcial0++;
+
+    alumnosReprobadosParcial2 = 0;
+    for(i=0; i<m; i++)
+        if(Calificaciones[i][2] < 6)
+            alumnosReprobadosParcial2++;
+
+    reprobadosFinal = 0;
+    for(i=0; i<m; i++)
+        if(Calificaciones[i][n] < 6)
+            reprobadosFinal++;
+
+
+    printf("Calif menor del semestre: %f, alumno %d, parcial %d\n", menor,alumnoMenor+1,parcialMenor+1);
+    printf("Calif mayor del semestre: %f, alumno %d, parcial %d\n", mayor,alumnoMayor+1,parcialMayor+1);
     printf("Calificaciones aprobatorias por parcial hay %d\n",aprobadosParcial);
     printf("Calificaciones aprobatorias por parcial hay %d\n",reprobadosParcial);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    printf("Hubo %d %f, durante el semestre\n",cuantasX,x);
+    printf("El alumno 2 aprobo %d parciales\n",parcialesAprobadosAlumno1);//Alumno (1+1)
+    printf("El alumno 4 reprobo %d parciales\n",parcialesReprobadosAlumno3);//Alumno (3+1)
+    printf("%d alumnos aprobaron el parcial 1\n",alumnosAprobadosParcial0);//Parcial (0+1)
+    printf("%d alumnos reprobaron el parcial 3\n",alumnosReprobadosParcial2);//Parcial (2+1)
+    printf("%d alumnos reprobaron el semestre\n",reprobadosFinal);//Columna n tiene calificaciones finales
 
     return 0;
 }
